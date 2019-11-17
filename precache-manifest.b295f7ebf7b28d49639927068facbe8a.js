5cdfa1254aacfb93ca86eabe225d3a7d0300feeb
Vlad Shum <shumanskyivlad@gmail.com>
2019-11-17 23:43:51 +0200

    Updates

---
+++ precache-manifest.b295f7ebf7b28d49639927068facbe8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "f24298302b45a605a6e4",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.777bb76b.chunk.js"
+  },
+  {
+    "revision": "d4f559cbf9b0ce677c9d",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.8b14927e.chunk.js"
+  },
+  {
+    "revision": "f24298302b45a605a6e4",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.22523364.chunk.css"
+  },
+  {
+    "revision": "58db06749d35eba9e8a9230e0e14ce02",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
